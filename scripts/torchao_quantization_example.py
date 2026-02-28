0666b95c6bdbe9fcc005084a5ca5ae4e95d41ca5
Gil Benezer <gil.benezer@gmail.com>
2026-02-28 12:40:09 -0500

    formatting

--- scripts/torchao_quantization_example.py
+++ scripts/torchao_quantization_example.py
@@ -2,14 +2,17 @@
 
 # necessary imports
 # external
-import os
-from pathlib import Path
 import copy
+import os
 import time
-import pandas as pd
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
 import plotly.express as px
 import torch
+# also try using the legacy torch facilities for static quantization
+import torch.ao.quantization as tq
 import torchao
 
 # internal
@@ -26,75 +29,54 @@ example_base_lightning_model = SuperconductorLightning.load_from_checkpoint(
 )
 example_base_model = example_base_lightning_model.model.eval()
 
-# make a couple of deep copies
-model_w16a16 = copy.deepcopy(example_base_model)
+# make a deep copy
 model_w8a8 = copy.deepcopy(example_base_model)
 
-# naively quantize the first model
-model_w16a16 = model_w16a16.to(dtype=torch.bfloat16)
-
-# quantize the second model with torchao to int8
+# quantize the model with torchao to int8
 torchao.quantization.quantize_(
     model_w8a8, config=torchao.quantization.Int8DynamicActivationInt8WeightConfig()
 )
 
 # save all the state dictionaries
 torch.save(example_base_model.state_dict(), "scripts/base_model.pth")
-torch.save(model_w16a16.state_dict(), "scripts/model_w16a16.pth")
 torch.save(model_w8a8.state_dict(), "scripts/model_w8a8.pth")
 
 # model sizes
 original_size = os.path.getsize("scripts/base_model.pth")
-size_16_bit = os.path.getsize("scripts/model_w16a16.pth")
 size_8_bit = os.path.getsize("scripts/model_w8a8.pth")
 
-print(f"Size reduction, 16 bit: {original_size / size_16_bit}")
 print(f"Size reduction, 8 bit: {original_size / size_8_bit}")
 
 # move all the models to the gpu
 if torch.cuda.is_available():
-
     # move the models to the GPU
     example_base_model.to(device=torch.device("cuda")).eval()
-    model_w16a16.to(device=torch.device("cuda")).eval()
     model_w8a8.to(device=torch.device("cuda")).eval()
-    
+
     # initialize arrays for storing inference times
     base_model_inference = np.zeros(shape=(1000,))
-    w16a16_model_inference = np.zeros(shape=(1000,))
     w8a8_model_inference = np.zeros(shape=(1000,))
-    
+
     torch.cuda.synchronize()
     for _ in range(10):
         example_inputs = torch.rand(size=(128, 81), device=torch.device("cuda"))
         _ = example_base_model(example_inputs)
-        _ = model_w16a16(example_inputs.to(dtype=torch.bfloat16))
         _ = model_w8a8(example_inputs)
-        
+
     torch.cuda.synchronize()
     for i in range(1000):
-        example_inputs = torch.rand(size=(128, 81), device=torch.device("cuda"), dtype=torch.float32)
+        example_inputs = torch.rand(
+            size=(128, 81), device=torch.device("cuda"), dtype=torch.float32
+        )
         start = time.time()
         _ = example_base_model(example_inputs)
         end = time.time()
         duration = end - start
         base_model_inference[i] = duration
-        
+
     base_inference_df = pd.DataFrame(data={"inference_time": base_model_inference})
     base_inference_df = base_inference_df.assign(model="base")
-        
-    torch.cuda.synchronize()
-    for i in range(1000):
-        example_inputs = torch.rand(size=(128, 81), device=torch.device("cuda"), dtype=torch.bfloat16)
-        start = time.time()
-        _ = model_w16a16(example_inputs)
-        end = time.time()
-        duration = end - start
-        w16a16_model_inference[i] = duration
-        
-    w16a16_inference_df = pd.DataFrame(data={"inference_time": w16a16_model_inference})
-    w16a16_inference_df = w16a16_inference_df.assign(model="w16a16")
-        
+
     torch.cuda.synchronize()
     for i in range(1000):
         example_inputs = torch.rand(size=(128, 81), device=torch.device("cuda"))
@@ -103,11 +85,13 @@ if torch.cuda.is_available():
         end = time.time()
         duration = end - start
         w8a8_model_inference[i] = duration
-        
+
     w8a8_inference_df = pd.DataFrame(data={"inference_time": w8a8_model_inference})
     w8a8_inference_df = w8a8_inference_df.assign(model="w8a8")
-    
-    inference_df = pd.concat([base_inference_df, w16a16_inference_df, w8a8_inference_df], axis=0)
-    
-    figure = px.box(data_frame=inference_df, x="model", y="inference_time", color="model")
+
+    inference_df = pd.concat([base_inference_df, w8a8_inference_df], axis=0)
+
+    figure = px.box(
+        data_frame=inference_df, x="model", y="inference_time", color="model"
+    )
     figure.write_html("scripts/inference_time_plots.html")
