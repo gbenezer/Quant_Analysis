3867511efec1df89eb24d7485c9a65931886973e
Gil Benezer <gil.benezer@gmail.com>
2026-03-01 21:16:38 -0500

    store the input and output dimension

--- models/simple_mlp.py
+++ models/simple_mlp.py
@@ -59,6 +59,10 @@ class SimpleMLP(nn.Module):
                 message=f"Activation type {specified_activation} is not valid. Defaulting to ReLU"
             )
             self.activation_cls = nn.ReLU
+            
+        # save the input and output dimensionality for later reference
+        self.input_dim = input_dim
+        self.output_dim = output_dim
 
         # construct the linear layer sequence adaptively
         self.linear_stack = nn.Sequential(
