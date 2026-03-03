9176bd2800b6253f0f33494a9d4346a6bfa623b4
Gil Benezer <gil.benezer@gmail.com>
2026-03-02 23:21:35 -0500

    added device handling

--- src/quant_analysis/metric_calculation/evaluate_mean_absolute_error.py
+++ src/quant_analysis/metric_calculation/evaluate_mean_absolute_error.py
@@ -6,12 +6,15 @@ from torch.utils.data import DataLoader
 def evaluate_mae(model: nn.Module, dataloader: DataLoader):
 
     model.eval()
+    device = next(model.parameters()).device
     num_samples = len(dataloader.dataset)
     absolute_error = 0.0
     loss_function = nn.L1Loss(reduction="sum")
 
     with torch.no_grad():
         for X, y in dataloader:
+            X = X.to(device)
+            y = y.to(device)
             pred = model(X)
             absolute_error += loss_function(pred, y).item()
 
