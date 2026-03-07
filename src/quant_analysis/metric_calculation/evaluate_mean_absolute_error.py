12c072067dce979d0154b8265c604e32fa1092e3
Gil Benezer <gil.benezer@gmail.com>
2026-03-07 16:28:32 -0500

    make the batch loading a bit more robust

--- src/quant_analysis/metric_calculation/evaluate_mean_absolute_error.py
+++ src/quant_analysis/metric_calculation/evaluate_mean_absolute_error.py
@@ -12,9 +12,9 @@ def evaluate_mae(model: nn.Module, dataloader: DataLoader):
     loss_function = nn.L1Loss(reduction="sum")
 
     with torch.no_grad():
-        for X, y in dataloader:
-            X = X.to(device)
-            y = y.to(device)
+        for batch in dataloader:
+            X = batch[0].to(device)
+            y = batch[1].to(device)
             pred = model(X)
             absolute_error += loss_function(pred, y).item()
 
