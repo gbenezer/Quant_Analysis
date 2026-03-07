3136fe23a133dbe743fe1966061fc51d394cb99d
Gil Benezer <gil.benezer@gmail.com>
2026-03-07 14:01:13 -0500

    changed the configuration metadata from a regular dictionary to a typed dictionary

--- src/quant_analysis/quantization/ptq/ptq_config_metadata.py
+++ src/quant_analysis/quantization/ptq/ptq_config_metadata.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict, NotRequired, TypedDict
+
+from torchao.core.config import AOBaseConfig
 from torchao.quantization import (
     Float8DynamicActivationFloat8WeightConfig,
     Float8DynamicActivationInt4WeightConfig,
@@ -8,73 +11,87 @@ from torchao.quantization import (
     Int8WeightOnlyConfig,
 )
 
-WEIGHT_ONLY_CONFIG_METADATA = {
-    "Int8WeightOnlyConfig": {
-        "ao_config": Int8WeightOnlyConfig,
-        "precision": "int8",
-        "bits_per_weight": 8,
-        "calibration": "static",
-        "weight_only": "yes",
-        "cuda_compute_capacity_compatibility": 8.6,
-        "ao_config_kwargs": {"version": 2},
-    },
-    "Float8WeightOnlyConfig": {
-        "ao_config": Float8WeightOnlyConfig,
-        "precision": "float8",
-        "bits_per_weight": 8,
-        "calibration": "static",
-        "weight_only": "yes",
-        "cuda_compute_capacity_compatibility": 8.6,
-        "ao_config_kwargs": {"version": 2},
-    },
-    "Int4WeightOnlyConfig": {
-        "ao_config": Int4WeightOnlyConfig,
-        "precision": "int4",
-        "bits_per_weight": 4,
-        "calibration": "static",
-        "weight_only": "yes",
-        "cuda_compute_capacity_compatibility": 8.6,
-        "ao_config_kwargs": {"version": 2},
-    },
+
+# define a TypedDict class for storing PTQ configurations and associated metadata
+class ConfigAndMetadataPTQ(TypedDict, total=True):
+    ao_config: type[AOBaseConfig]
+    precision: str
+    bits_per_weight: int
+    dynamic_calibration: bool
+    weight_only: bool
+    cuda_compute_capacity_compatibility: float
+    ao_config_kwargs: NotRequired[Dict[str, Any]]
+
+
+PTQ_WEIGHT_ONLY_CONFIG_METADATA = {
+    "Int8WeightOnlyConfig": ConfigAndMetadataPTQ(
+        ao_config=Int8WeightOnlyConfig,
+        precision="int8",
+        bits_per_weight=8,
+        dynamic_calibration=False,
+        weight_only=True,
+        cuda_compute_capacity_compatibility=8.6,
+        ao_config_kwargs={"version": 2},
+    ),
+    "Float8WeightOnlyConfig": ConfigAndMetadataPTQ(
+        ao_config=Float8WeightOnlyConfig,
+        precision="float8",
+        bits_per_weight=8,
+        dynamic_calibration=False,
+        weight_only=True,
+        cuda_compute_capacity_compatibility=8.6,
+        ao_config_kwargs={"version": 2},
+    ),
+    "Int4WeightOnlyConfig": ConfigAndMetadataPTQ(
+        ao_config=Int4WeightOnlyConfig,
+        precision="int4",
+        bits_per_weight=4,
+        dynamic_calibration=False,
+        weight_only=True,
+        cuda_compute_capacity_compatibility=8.6,
+        ao_config_kwargs={"version": 2},
+    ),
 }
 
-WEIGHT_ACTIVATION_CONFIG_METADATA = {
-    "Float8DynamicActivationFloat8WeightConfig": {
-        "ao_config": Float8DynamicActivationFloat8WeightConfig,
-        "precision": "float8",
-        "bits_per_weight": 8,
-        "calibration": "dynamic",
-        "weight_only": "no",
-        "cuda_compute_capacity_compatibility": 8.6,
-        "ao_config_kwargs": {"version": 2},
-    },
-    "Float8StaticActivationFloat8WeightConfig": {
-        "ao_config": Float8StaticActivationFloat8WeightConfig,
-        "precision": "float8",
-        "bits_per_weight": 8,
-        "calibration": "static",
-        "weight_only": "no",
-        "cuda_compute_capacity_compatibility": 8.9,
-        "ao_config_kwargs": {},
-    },
-    "Int8DynamicActivationInt8WeightConfig": {
-        "ao_config": Int8DynamicActivationInt8WeightConfig,
-        "precision": "int8",
-        "bits_per_weight": 8,
-        "calibration": "dynamic",
-        "weight_only": "no",
-        "cuda_compute_capacity_compatibility": 8.6,
-        "ao_config_kwargs": {"version": 2},
-    },
-    "Float8DynamicActivationInt4WeightConfig": {
-        "ao_config": Float8DynamicActivationInt4WeightConfig,
-        "precision": "float8act_int4weight",
-        "bits_per_weight": 4,
-        "calibration": "dynamic",
-        "weight_only": "no",
-        "cuda_compute_capacity_compatibility": 8.6,
-        "ao_config_kwargs": {"version": 2},
-    },
+PTQ_WEIGHT_ACTIVATION_CONFIG_METADATA = {
+    "Float8DynamicActivationFloat8WeightConfig": ConfigAndMetadataPTQ(
+        ao_config=Float8DynamicActivationFloat8WeightConfig,
+        precision="float8",
+        bits_per_weight=8,
+        dynamic_calibration=True,
+        weight_only=False,
+        cuda_compute_capacity_compatibility=8.6,
+        ao_config_kwargs={"version": 2},
+    ),
+    "Float8StaticActivationFloat8WeightConfig": ConfigAndMetadataPTQ(
+        ao_config=Float8StaticActivationFloat8WeightConfig,
+        precision="float8",
+        bits_per_weight=8,
+        dynamic_calibration=False,
+        weight_only=False,
+        cuda_compute_capacity_compatibility=8.9,
+        ao_config_kwargs={},
+    ),
+    "Int8DynamicActivationInt8WeightConfig": ConfigAndMetadataPTQ(
+        ao_config=Int8DynamicActivationInt8WeightConfig,
+        precision="int8",
+        bits_per_weight=8,
+        dynamic_calibration=True,
+        weight_only=False,
+        cuda_compute_capacity_compatibility=8.6,
+        ao_config_kwargs={"version": 2},
+    ),
+    "Float8DynamicActivationInt4WeightConfig": ConfigAndMetadataPTQ(
+        ao_config=Float8DynamicActivationInt4WeightConfig,
+        precision="float8act_int4weight",
+        bits_per_weight=4,
+        dynamic_calibration=True,
+        weight_only=False,
+        cuda_compute_capacity_compatibility=8.6,
+        ao_config_kwargs={"version": 2},
+    ),
 }
 
-QUANT_CONFIG_METADATA = WEIGHT_ONLY_CONFIG_METADATA | WEIGHT_ACTIVATION_CONFIG_METADATA
+PTQ_QUANT_CONFIG_METADATA = (
+    PTQ_WEIGHT_ONLY_CONFIG_METADATA | PTQ_WEIGHT_ACTIVATION_CONFIG_METADATA
+)
