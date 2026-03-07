f3980a33290d19e916e63379eacedf8cbd72f2e5
Gil Benezer <gil.benezer@gmail.com>
2026-03-07 18:27:09 -0500

    processed the dataframe to be more amenable to downstream analysis

--- src/quant_analysis/data_processing/ptq_result_to_dataframe.py
+++ src/quant_analysis/data_processing/ptq_result_to_dataframe.py
@@ -26,4 +26,26 @@ def ptq_results_to_dataframe(results):
 
         rows.append(row)
 
-    return pd.DataFrame(rows)
+    raw_dataframe = pd.DataFrame(rows)
+    melted_dataframe = raw_dataframe.melt(
+        id_vars=[
+            "config_name",
+            "precision",
+            "bits_per_weight",
+            "dynamic_calibration",
+            "weight_only",
+        ],
+        var_name="metric",
+        value_name="value",
+    )
+    decomposed_metric_df = melted_dataframe["metric"].str.extract(
+        r"^(pytorch|onnx|pt2)_(quantized|relative)_(.*)$"
+    )
+    decomposed_metric_df.columns = ["runtime", "relative_metric", "base_metric"]
+    decomposed_metric_df["relative"] = (
+        decomposed_metric_df["relative_metric"] == "relative"
+    )
+    decomposed_metric_df.drop(labels=["relative_metric"], inplace=True, axis=1)
+    output_df = pd.concat([melted_dataframe, decomposed_metric_df], axis=1)
+
+    return output_df
