43fe3ac278ac64e67d897dc3ebf9670fa464ad77
Gil Benezer <gil.benezer@gmail.com>
2026-03-20 00:45:46 -0400

    proper indentation

--- scripts/model_cluster_experiment.py
+++ scripts/model_cluster_experiment.py
@@ -18,125 +18,125 @@ from src.quant_analysis.quantization.ptq.run_ptq import run_ptq, run_ptq_isolate
 if __name__ = "__main__":
     mp.set_start_method("spawn", force=True)
 
-# Define globals for script
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
-OUTPUT_PATH = Path.cwd() / "data" / "output" / "csv"
-OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
-NUMBER_TRAINING_EPOCHS = 25
-NUMBER_TRAINING_RUNS = 10
-NUMBER_EVALUATE_RUNS = 10
-base_config = SimpleMLPConfig(
-    input_dim=81,
-    output_dim=1,
-    neurons_per_layer=[512, 256, 128],
-    activation="gelu",
-    use_batch_norm=True,
-)
-# Intentional to evaluate variance
-SEED = None
-
-# dictionary to properly feed the 
-DATALOADER_KWARGS = dict(
-    test_fraction=0.2, random_seed=SEED, n_workers=4, batch_n=128
-)
-
-full_ptq_dataframe_list = []
-weight_only_ptq_dataframe_list = []
-
-for train_run in range(NUMBER_TRAINING_RUNS):
-    print(f"training model in training run {train_run + 1}")
-    test_model = construct_mlp(
-        config=base_config,
-        name=f"base_model_FP32_train_run_{train_run + 1}",
-        max_epochs=NUMBER_TRAINING_EPOCHS,
-        seed=SEED,
+    # Define globals for script
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    OUTPUT_PATH = Path.cwd() / "data" / "output" / "csv"
+    OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+    NUMBER_TRAINING_EPOCHS = 25
+    NUMBER_TRAINING_RUNS = 10
+    NUMBER_EVALUATE_RUNS = 10
+    base_config = SimpleMLPConfig(
+        input_dim=81,
+        output_dim=1,
+        neurons_per_layer=[512, 256, 128],
+        activation="gelu",
+        use_batch_norm=True,
     )
-    test_model.share_memory()  # required for passing model to subprocess
+    # Intentional to evaluate variance
+    SEED = None
 
-    for eval_run in range(NUMBER_EVALUATE_RUNS):
-        print(
-            f"getting evaluation data for training run {train_run + 1}, evaluation run {eval_run + 1}"
-        )
-        (
-            _,
-            _,
-            _,
-            _,
-            train_loader,
-            _,
-            test_loader,
-        ) = get_superconductivity_data(
-            **DATALOADER_KWARGS
-        )
+    # dictionary to properly feed the 
+    DATALOADER_KWARGS = dict(
+        test_fraction=0.2, random_seed=SEED, n_workers=4, batch_n=128
+    )
 
-        print("running ptq with size estimation on all configurations, train dataset")
-        train_loader_full_output = run_ptq_isolated(
-            base_model=test_model,
-            dataloader_kwargs=DATALOADER_KWARGS,
-            evaluation_device=str(device),  # spawn can't pickle torch.device
-            batch_size=128,
-            weight_only=False,
-        )
-        train_loader_full_df = ptq_results_to_dataframe(train_loader_full_output)
-        train_loader_full_df = train_loader_full_df.assign(
-            train_run=(train_run + 1), eval_run=(eval_run + 1), split="train"
-        )
-        full_ptq_dataframe_list.append(train_loader_full_df)
+    full_ptq_dataframe_list = []
+    weight_only_ptq_dataframe_list = []
 
-        print("running ptq with size estimation on all configurations, test dataset")
-        test_loader_full_output = run_ptq_isolated(
-            base_model=test_model,
-            dataloader_kwargs=DATALOADER_KWARGS,
-            evaluation_device=str(device),
-            batch_size=128,
-            weight_only=True,
+    for train_run in range(NUMBER_TRAINING_RUNS):
+        print(f"training model in training run {train_run + 1}")
+        test_model = construct_mlp(
+            config=base_config,
+            name=f"base_model_FP32_train_run_{train_run + 1}",
+            max_epochs=NUMBER_TRAINING_EPOCHS,
+            seed=SEED,
         )
-        test_loader_full_df = ptq_results_to_dataframe(test_loader_full_output)
-        test_loader_full_df = test_loader_full_df.assign(
-            train_run=(train_run + 1), eval_run=(eval_run + 1), split="test"
-        )
-        full_ptq_dataframe_list.append(test_loader_full_df)
+        test_model.share_memory()  # required for passing model to subprocess
 
-        print(
-            "running ptq, weight only configurations, actual size measurement, train set"
-        )
-        train_loader_weight_output = run_ptq(
-            base_model=test_model,
-            dataloader=train_loader,
-            evaluation_device=device,
-            batch_size=128,
-            print_debug=True,
-            weight_only=True,
-        )
-        train_loader_weight_df = ptq_results_to_dataframe(train_loader_weight_output)
-        train_loader_weight_df = train_loader_weight_df.assign(
-            train_run=(train_run + 1), eval_run=(eval_run + 1), split="train"
-        )
-        weight_only_ptq_dataframe_list.append(train_loader_weight_df)
+        for eval_run in range(NUMBER_EVALUATE_RUNS):
+            print(
+                f"getting evaluation data for training run {train_run + 1}, evaluation run {eval_run + 1}"
+            )
+            (
+                _,
+                _,
+                _,
+                _,
+                train_loader,
+                _,
+                test_loader,
+            ) = get_superconductivity_data(
+                **DATALOADER_KWARGS
+            )
 
-        print(
-            "running ptq, weight only configurations, actual size measurement, test set"
-        )
-        test_loader_weight_output = run_ptq(
-            base_model=test_model,
-            dataloader=test_loader,
-            evaluation_device=device,
-            batch_size=128,
-            print_debug=True,
-            weight_only=True,
-        )
-        test_loader_weight_df = ptq_results_to_dataframe(test_loader_weight_output)
-        test_loader_weight_df = test_loader_weight_df.assign(
-            train_run=(train_run + 1), eval_run=(eval_run + 1), split="test"
-        )
-        weight_only_ptq_dataframe_list.append(test_loader_weight_df)
+            print("running ptq with size estimation on all configurations, train dataset")
+            train_loader_full_output = run_ptq_isolated(
+                base_model=test_model,
+                dataloader_kwargs=DATALOADER_KWARGS,
+                evaluation_device=str(device),  # spawn can't pickle torch.device
+                batch_size=128,
+                weight_only=False,
+            )
+            train_loader_full_df = ptq_results_to_dataframe(train_loader_full_output)
+            train_loader_full_df = train_loader_full_df.assign(
+                train_run=(train_run + 1), eval_run=(eval_run + 1), split="train"
+            )
+            full_ptq_dataframe_list.append(train_loader_full_df)
 
-full_ptq_dataframe = pd.concat(full_ptq_dataframe_list)
-weight_only_ptq_dataframe = pd.concat(weight_only_ptq_dataframe_list)
+            print("running ptq with size estimation on all configurations, test dataset")
+            test_loader_full_output = run_ptq_isolated(
+                base_model=test_model,
+                dataloader_kwargs=DATALOADER_KWARGS,
+                evaluation_device=str(device),
+                batch_size=128,
+                weight_only=True,
+            )
+            test_loader_full_df = ptq_results_to_dataframe(test_loader_full_output)
+            test_loader_full_df = test_loader_full_df.assign(
+                train_run=(train_run + 1), eval_run=(eval_run + 1), split="test"
+            )
+            full_ptq_dataframe_list.append(test_loader_full_df)
+
+            print(
+                "running ptq, weight only configurations, actual size measurement, train set"
+            )
+            train_loader_weight_output = run_ptq(
+                base_model=test_model,
+                dataloader=train_loader,
+                evaluation_device=device,
+                batch_size=128,
+                print_debug=True,
+                weight_only=True,
+            )
+            train_loader_weight_df = ptq_results_to_dataframe(train_loader_weight_output)
+            train_loader_weight_df = train_loader_weight_df.assign(
+                train_run=(train_run + 1), eval_run=(eval_run + 1), split="train"
+            )
+            weight_only_ptq_dataframe_list.append(train_loader_weight_df)
+
+            print(
+                "running ptq, weight only configurations, actual size measurement, test set"
+            )
+            test_loader_weight_output = run_ptq(
+                base_model=test_model,
+                dataloader=test_loader,
+                evaluation_device=device,
+                batch_size=128,
+                print_debug=True,
+                weight_only=True,
+            )
+            test_loader_weight_df = ptq_results_to_dataframe(test_loader_weight_output)
+            test_loader_weight_df = test_loader_weight_df.assign(
+                train_run=(train_run + 1), eval_run=(eval_run + 1), split="test"
+            )
+            weight_only_ptq_dataframe_list.append(test_loader_weight_df)
+
+    full_ptq_dataframe = pd.concat(full_ptq_dataframe_list)
+    weight_only_ptq_dataframe = pd.concat(weight_only_ptq_dataframe_list)
 
 
-full_ptq_dataframe.to_csv((OUTPUT_PATH / "full_ptq_baseline_experiment_data.csv"))
-weight_only_ptq_dataframe.to_csv(
-    (OUTPUT_PATH / "weight_only_ptq_baseline_experiment_data.csv")
-)
+    full_ptq_dataframe.to_csv((OUTPUT_PATH / "full_ptq_baseline_experiment_data.csv"))
+    weight_only_ptq_dataframe.to_csv(
+        (OUTPUT_PATH / "weight_only_ptq_baseline_experiment_data.csv")
+    )
