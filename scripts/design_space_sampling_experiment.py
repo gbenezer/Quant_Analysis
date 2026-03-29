e5779600d95f4cadd4af91bd5d55b233ef165579
Gil Benezer <gil.benezer@gmail.com>
2026-03-29 13:06:52 -0400

    formatting and checkpointing added

--- scripts/design_space_sampling_experiment.py
+++ scripts/design_space_sampling_experiment.py
@@ -73,6 +73,44 @@ model_configs = {
     test_fraction=0.2, random_seed=SEED, n_workers=4, batch_n=BATCH_SIZE
 )
 
+
+# create helper function for CSV writing
+def write_csvs():
+    if full_ptq_dataframe_list:
+        full_ptq_dataframe = pd.concat(full_ptq_dataframe_list)
+        full_ptq_experiment_df = pd.merge(
+            left=model_config_dataframe,
+            right=full_ptq_dataframe,
+            how="outer",
+            on="model_ID",
+            suffixes=("_config", "_result"),
+            validate="one_to_many",
+        )
+        full_ptq_experiment_df.to_csv(
+            OUTPUT_PATH
+            / f"full_ptq_config_sampling_experiment_data_{EXPERIMENT_DEVICE}.csv"
+        )
+    else:
+        print("No full PTQ results collected.")
+
+    if weight_only_ptq_dataframe_list:
+        weight_only_ptq_dataframe = pd.concat(weight_only_ptq_dataframe_list)
+        weight_only_ptq_experiment_df = pd.merge(
+            left=model_config_dataframe,
+            right=weight_only_ptq_dataframe,
+            how="outer",
+            on="model_ID",
+            suffixes=("_config", "_result"),
+            validate="one_to_many",
+        )
+        weight_only_ptq_experiment_df.to_csv(
+            OUTPUT_PATH
+            / f"weight_only_ptq_config_sampling_experiment_data_{EXPERIMENT_DEVICE}.csv"
+        )
+    else:
+        print("No weight-only PTQ results collected.")
+
+
 if __name__ == "__main__":
     for config_ID, config in model_configs.items():
         test_config_model = construct_mlp(
@@ -119,37 +157,8 @@ if __name__ == "__main__":
                 config_weight_only_output_df = config_weight_only_output_df.assign(
                     model_ID=config_ID, eval_run=(eval_run + 1), weight_only=True
                 )
+                weight_only_ptq_dataframe_list.append(config_weight_only_output_df)
 
-    if full_ptq_dataframe_list:
-        full_ptq_dataframe = pd.concat(full_ptq_dataframe_list)
-        full_ptq_experiment_df = pd.merge(
-            left=model_config_dataframe,
-            right=full_ptq_dataframe,
-            how="outer",
-            on="model_ID",
-            suffixes=("_config", "_result"),
-            validate="one_to_many",
-        )
-        full_ptq_experiment_df.to_csv(
-            OUTPUT_PATH
-            / f"full_ptq_config_sampling_experiment_data_{EXPERIMENT_DEVICE}.csv"
-        )
-    else:
-        print("No full PTQ results collected.")
+        write_csvs()
 
-    if weight_only_ptq_dataframe_list:
-        weight_only_ptq_dataframe = pd.concat(weight_only_ptq_dataframe_list)
-        weight_only_ptq_experiment_df = pd.merge(
-            left=model_config_dataframe,
-            right=weight_only_ptq_dataframe,
-            how="outer",
-            on="model_ID",
-            suffixes=("_config", "_result"),
-            validate="one_to_many",
-        )
-        weight_only_ptq_experiment_df.to_csv(
-            OUTPUT_PATH
-            / f"weight_only_ptq_config_sampling_experiment_data_{EXPERIMENT_DEVICE}.csv"
-        )
-    else:
-        print("No weight-only PTQ results collected.")
+    write_csvs()
